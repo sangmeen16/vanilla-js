2a711903b525ca8c337914b2f7eaedb28b84f383
sangmeen16 <sangmeen16@gmail.com>
2022-10-01 00:22:47 +0900

    Modify
    - 기존 이벤트핸들러 삭제
    - h2 tag 클릭시 2가지색 변화

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-
 const title = document.querySelector("h2");
-const superEventHandler = {
-  handleMouseEnter: function () {
-    title.innerText = "Mouse is here!";
-    title.style.color = colors[0];
-  },
-  handleMouseLeave: function () {
-    title.innerText = "Mouse is gone!";
-    title.style.color = colors[1];
-  },
-  handleResize: function () {
-    title.innerText = "You just resized";
-    title.style.color = colors[2];
-  },
-  handleContextMenu: function () {
-    title.innerText = "That was a right click!";
-    title.style.color = colors[4];
-  },
-};
 
-title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
-title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
-window.addEventListener("resize", superEventHandler.handleResize);
-window.addEventListener("contextmenu", superEventHandler.handleContextMenu);
+function handleTitleClick() {
+  const currentColor = title.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+
+  title.style.color = newColor;
+}
+
+title.addEventListener("click", handleTitleClick);
