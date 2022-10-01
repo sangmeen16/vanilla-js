b132fac8197649493ce9c9ed423628ed8b0ff57f
sangmeen16 <sangmeen16@gmail.com>
2022-10-01 23:45:38 +0900

    Modify
    - 이벤트 핸들러 추가
     : 마우스 클릭, hover, non-hover, resize
    
    - 마우스 클릭 이벤트 다양한 방법 추가

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const bd = document.querySelector("body");
+const title = document.querySelector(".bd .hello h1");
+
+function handleTitleClick1() {
+  const currentColor = title.style.color;
+  let newColor;
+
+  if (currentColor === "tomato") {
+    newColor = "cornflowerblue";
+  } else {
+    newColor = "tomato";
+  }
+  title.style.color = newColor;
+}
+
+function handleTitleClick2() {
+  const clickedClass = "clicked";
+  if (title.className === clickedClass) {
+    title.className = "";
+  } else {
+    title.className = clickedClass;
+  }
+}
 
 function handleResize() {
   const size = window.innerWidth;
+
   if (size >= 1200) {
     bd.style.backgroundColor = "orange";
   } else if (size < 1200 && size > 900) {
@@ -11,4 +34,17 @@ function handleResize() {
   }
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is leave!";
+}
+
 window.addEventListener("resize", handleResize);
+
+// title.addEventListener("click", handleTitleClick1);
+title.addEventListener("click", handleTitleClick2);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
