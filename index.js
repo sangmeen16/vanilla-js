f29f9a892e19f688aae1400edc71d141b550a603
sangmeen16 <sangmeen16@gmail.com>
2022-10-03 21:21:50 +0900

    Modify
    1. Input 범위 필터 설정
     - maxNumber, guessNumber
    
    2. 랜덤값 생성
    
    3. guessNumber값과 machineNumber값 비교

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const gameForm = document.querySelector("#gameForm");
+const maxNumInput = document.querySelector("#maxNumber");
+const guessNumInput = document.querySelector("#guessNumber");
+const score = document.querySelector("#score");
+const result = document.querySelector("#result");
+const resultArea = document.querySelector("#resultArea");
 
 const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  greeting.innerText = `hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+function getRandomInt(max) {
+  const result1 = Array.from({ length: max }, () =>
+    Math.floor(Math.random() * max)
+  );
+  result1.push(parseInt(max));
+  const result2 = result1[Math.floor(Math.random() * result1.length)];
+
+  return result2;
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function onLoginSubmit(event) {
+  event.preventDefault();
+
+  let maxNumber = maxNumInput.value;
+  let guessNumber = guessNumInput.value;
+  let machineNumber = getRandomInt(maxNumber);
+
+  if (maxNumber === "" || guessNumber === "") {
+    alert("값을 입력해주세요");
+    resultArea.classList.add(HIDDEN_CLASSNAME);
+  } else if (maxNumber <= 0) {
+    alert("최대값을 0보다 크게 입력해주세요");
+    resultArea.classList.add(HIDDEN_CLASSNAME);
+  } else if (guessNumber < 0) {
+    alert("추측값을 0이상 입력해주세요");
+    resultArea.classList.add(HIDDEN_CLASSNAME);
+  } else if (maxNumber < guessNumber) {
+    alert("추측값이 최대값보다 큽니다!!");
+    resultArea.classList.add(HIDDEN_CLASSNAME);
+  } else {
+    resultArea.classList.remove(HIDDEN_CLASSNAME);
+  }
+  score.innerText = `You chose: ${guessNumber}, the machine chose: ${machineNumber}`;
+
+  if (machineNumber != guessNumber) {
+    result.innerText = "You Lose";
+  } else {
+    result.innerText = "You Win";
+  }
+}
+
+gameForm.addEventListener("submit", onLoginSubmit);
