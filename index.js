22799e442474af401a7a787893007ce33116af04
sangmeen16 <sangmeen16@gmail.com>
2022-10-03 18:34:36 +0900

    Add : Input tag 값 가져오기 예제

--- index.js
+++ index.js
@@ -1,82 +1,15 @@
-const bd = document.querySelector("body");
-const title = document.querySelector(".bd .hello h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const superEventHandler = {
-  handleTitleClick1: function () {
-    const currentColor = title.style.color;
-    let newColor;
+const HIDDEN_CLASSNAME = "hidden";
 
-    if (currentColor === "tomato") {
-      newColor = "cornflowerblue";
-    } else {
-      newColor = "tomato";
-    }
-    title.style.color = newColor;
-  },
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-  handleTitleClick2: function () {
-    const clickedClass = "clicked";
-    if (title.className === clickedClass) {
-      title.className = "";
-    } else {
-      title.className = clickedClass;
-    }
-  },
-
-  handleTitleClick3: function () {
-    const clickedClass = "clicked";
-    if (title.classList.contains(clickedClass)) {
-      title.classList.remove(clickedClass);
-    } else {
-      title.classList.add(clickedClass);
-    }
-  },
-
-  handleTitleClick4: function () {
-    title.classList.toggle("clicked");
-  },
-
-  handleResize_1: function () {
-    const size = window.innerWidth;
-
-    if (size >= 1200) {
-      bd.style.backgroundColor = "orange";
-    } else if (size < 1200 && size > 900) {
-      bd.style.backgroundColor = "purple";
-    } else {
-      bd.style.backgroundColor = "deepskyblue";
-    }
-  },
-
-  handleResize_2: function () {
-    const size = window.innerWidth;
-    const BIG_SCREEN = "bigScreen";
-    const MIDIUM_SCREEN = "mediumScreen";
-    const SMALL_SCREEN = "smallScreen";
-
-    if (size >= 1200) {
-      bd.classList.add(BIG_SCREEN);
-      bd.classList.remove(MIDIUM_SCREEN);
-    } else if (size < 1200 && size > 900) {
-      bd.classList.add(MIDIUM_SCREEN);
-      bd.classList.remove(SMALL_SCREEN);
-    } else {
-      bd.classList.add(SMALL_SCREEN);
-      bd.classList.remove(MIDIUM_SCREEN);
-    }
-  },
-
-  handleMouseEnter: function () {
-    title.innerText = "Mouse is here!";
-  },
-
-  handleMouseLeave: function () {
-    title.innerText = "Mouse is leave!";
-  },
-};
-
-window.addEventListener("resize", superEventHandler.handleResize_2);
-
-title.addEventListener("click", superEventHandler.handleTitleClick4);
-title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
-title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
+loginForm.addEventListener("submit", onLoginSubmit);
